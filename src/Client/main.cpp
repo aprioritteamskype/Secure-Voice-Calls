07c7bd780f0b4408a89a0033d1e663da405f0c93
developernevid <developer.nevid@gmail.com>
2019-05-04 17:22:10 +0300

    [Client] gRPC integration
    TODO: execute command conan install <PATH to conanfile.txt>
    in build directory

--- src/Client/main.cpp
+++ src/Client/main.cpp
@@ -1,5 +1,10 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <thread>
+
+#include "client.h"
+
+void runCLient();
 
 int main(int argc, char *argv[])
 {
@@ -12,5 +17,20 @@ int main(int argc, char *argv[])
     if (engine.rootObjects().isEmpty())
         return -1;
 
+    std::thread clientThread(runCLient);
+    clientThread.join();
     return app.exec();
+}
+
+void runCLient(){
+    std::string address("0.0.0.0:5000");
+        secure_voice_call::Client client(
+            grpc::CreateChannel(
+                address,
+                grpc::InsecureChannelCredentials()
+            )
+        );
+
+        std::string response = client.sendRequest("CLIENT__ nevatest");
+        std::cout << "Answer received: " << response << std::endl;
 }
